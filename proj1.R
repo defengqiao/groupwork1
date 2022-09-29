962ab0b6fade0ce226dadb62cd2b3cbff0ea7e42
Defeng Qiao <qiaodefeng@foxmail.com>
2022-09-29 19:45:46 +0100

    the first 5 steps are completed

--- proj1.R
+++ proj1.R
@@ -1,3 +1,6 @@
+#Defeng Qiao, s2419769; YiZhou Chen, s2450877;Tianai Ren,
+
+
 #1.
 
 
@@ -5,13 +8,51 @@
 
 
 #3.
-
+setwd("D:/Study/R_Code/groupwork1")
+a <- scan("pg10.txt",what="character",skip=104) ## skip contents
+n <- length(a)
+a <- a[-((n-2886):n)] ## strip license
+a <- a[-grep("[0123456789]:[0123456789]",a)] ## strip out verse numbers
 
 #4.
-
-
+#a<-c("aa,","bb.","cc?","dd!") #used for test
+split_punct <- function(a){
+  for (i in c(",",".",";","!",":","?")) {
+    
+    if (i=="."){
+      ii <- grep("\\.",a)               #indices of "." in a; add "\\" in front of "?" and "." to prevent error
+      if (length(ii)){                  #If the "." is not in the words，it will lead to errors
+        a <- gsub("\\.","",a)           #remove "." in the words
+        xs<-rep(0,length(ii)+length(a)) #vector to store the words and separated marks
+        iis<- ii+1:length(ii)           #where should "." go in xs?
+        xs[iis]<-"."                    #insert "."
+        xs[-iis]<-a                     #insert the rest units
+        a<-xs                           #give value back to a
+      }
+    } else if (i=="?") {               #similar to "."
+      ii <- grep("\\?",a)         
+      a <- gsub("\\?","",a)       
+      xs<-rep(0,length(ii)+length(a))
+      iis<- ii+1:length(ii)
+      xs[iis]<-"?"
+      xs[-iis]<-a
+      a<-xs
+    }else{                             #rest of the punctuation marks
+      ii <- grep(i,a)         
+      if (length(ii)){
+        a <- gsub(i,"",a)       
+        xs<-rep(0,length(ii)+length(a))
+        iis<- ii+1:length(ii)
+        xs[iis]<-i
+        xs[-iis]<-a
+        a<-xs
+      }
+    }
+  }
+  return(a)            #return the separated a
+}
 #5.
-
+a<-split_punct(a)      #separate the punctuation marks
 
 #6.
 
