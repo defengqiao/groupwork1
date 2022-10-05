b6f104f277861038ae50c997399c9c95f2b94c2b
RenTianai <s2329207@ed.ac.uk>
2022-10-05 01:52:51 +0100

    add appropriate comment for whole R code

--- proj1.R
+++ proj1.R
@@ -14,28 +14,32 @@ rm(list = ls())
 
 
 #3.
+#Read the  file into R and pre-processing of data
 setwd("D:/Study/R_Code/groupwork1")
+setwd("C:/Users/Renti/Documents/Ren Tianai/postgraduate/sem 1/SP R")
 a <- scan("pg10.txt",what="character",skip=104) ## skip contents
 n <- length(a)
 a <- a[-((n-2886):n)] ## strip license
 a <- a[-grep("[0123456789]:[0123456789]",a)] ## strip out verse numbers
 
 #4.
+#
 #a<-c("aa,","bb.","cc?","dd!") #used for test
-split_punct <- function(a){
-  for (i in c(",",".",";","!",":","?")) {
+#split_punct is used to search for each word that contains a punctuation mark, separating it from the symbol and placing it appropriately.
+split_punct <- function(a){ #Input is text a
+  for (i in c(",",".",";","!",":","?")) {#"." and "?" are special characters in regular expressions which are treated separately
     
     if (i=="."){
-      ii<-grep("\\.",a)               #indices of "." in a; add "\\" in front of "?" and "." to prevent error
+      ii<-grep("\\.",a)                 #indices of "." in a; add "\\" in front of "?" and "." to prevent error
       if (length(ii)){                  #If the "." is not in the words，it will lead to errors
-        a<-gsub("\\.","",a)           #remove "." in the words
+        a<-gsub("\\.","",a)             #remove "." in the words
         xs<-rep(0,length(ii)+length(a)) #vector to store the words and separated marks
-        iis<-ii+1:length(ii)           #where should "." go in xs?
+        iis<-ii+1:length(ii)            #where should "." go in xs?
         xs[iis]<-"."                    #insert "."
         xs[-iis]<-a                     #insert the rest units
         a<-xs                           #give value back to a
       }
-    } else if (i=="?") {               #similar to "."
+    } else if (i=="?") {                #similar to "."
       ii<-grep("\\?",a)
       if (length(ii)){ 
         a<-gsub("\\?","",a)       
@@ -57,12 +61,13 @@ split_punct <- function(a){
       }
     }
   }
-  return(a)            #return the separated a
+  return(a)              # Output is a being separated
 }
 #5.
-a <- split_punct(a)      #separate the punctuation marks
+a <- split_punct(a)      #a contains the separate word and punctuation mark
 
 #6.
+#Find the 500 most commonly occurring words and put them into order
 #replace the capital letters with lower case letter
 al <- tolower(a)
 #and find the vector of unique words
@@ -75,44 +80,47 @@ index<-match(al,b)
 times <- tabulate(index)
 
 #sort by number of times
-ordern<-sort(times,decreasing = T)  #number of times
+ordern<-sort(times,decreasing = T)  #number of times sort by reverse order
 orderi<-order(times,decreasing = T) #index
 
 #common words
-icom<-orderi[1:500]  #前500单词在b中的位置
-com<-b[icom]  #按次数排列的前500单词
+icom<-orderi[1:500]   #position of the 500 most common words in the vector of unique words
+com<-b[icom]          #Put these 500 words in order of frequency into the vector 'com'
 
 #7.
-comi<-match(al,com)#each element of the full text vector corresponds to the common words
+#first column is the index of common words
+comi<-match(al,com)   #each element of the full text vector corresponds to the common words
 
-comn<-rep(NA,length(comi))    #next common
-comn[2:length(comi)]<-comi[1:(length(comi)-1)]
+#next column is the index for the following words
+comn<-rep(NA,length(comi))                      #Initialize next words index vector
+comn[2:length(comi)]<-comi[1:(length(comi)-1)]  #the vector shifted by one place
 
-comf<-rep(NA,length(comi))    #final common
-comf[3:length(comi)]<-comi[1:(length(comi)-2)]
+#first column is the index of the final words
+comf<-rep(NA,length(comi))                      #Initialize final words index vector
+comf[3:length(comi)]<-comi[1:(length(comi)-2)]  #the vector shifted by one place
 
 tr<-cbind(comi,comn)
 
-#prepare for A
+#prepare for matrix A
 sumr<-rowSums(tr)             #the sum of every row that have NA is NA
 sumT<-!is.na(sumr)            #turn NA to FALSE, the rest is TRUE
-tr2<-tr[sumT,]               #find all common words double which dont have NA
+tr2<-tr[sumT,]                #find all common words double which donot have NA
 
-#create A
-A <- array(0,c(length(com),length(com))) #initialize A
+#create matrix A
+A <- array(0,c(length(com),length(com)))  #initialize A
 for (i in 1:dim(tr2)[1]) {
   A[tr2[i,1],tr2[i,2]]<-A[tr2[i,1],tr2[i,2]]+1
 }
 
 tr<-cbind(tr,comf)            #common words triplets
 
-#prepare for T
+#prepare for array T
 sumr<-rowSums(tr)             #the sum of every row that have NA is NA
 sumT<-!is.na(sumr)            #turn NA to FALSE, the rest is TRUE
-tr3<-tr[sumT,]                 #find all common words triplets which dont have NA
+tr3<-tr[sumT,]                #find all common words triplets which dont have NA
 
-#create T!!!!
-T <- array(0,c(length(com),length(com),length(com))) #initialize T
+#create T
+T <- array(0,c(length(com),length(com),length(com)))  #initialize T
 for (i in 1:dim(tr3)[1]) {
   T[tr3[i,1],tr3[i,2],tr3[i,3]]<-T[tr3[i,1],tr3[i,2],tr3[i,3]]+1
 }
@@ -121,21 +129,22 @@ for (i in 1:dim(tr3)[1]) {
 S <- ordern[1:500]
 
 #8.
-#a function select word
-te <- rep(0,50)
+#Simulate 50 words sections from the model
+te <- rep(0,50)  
 w <- rep(0,50)
 
-write1 <- function(S){                           #S
+#write function is to pick word from A,S,T based on word and the probabilities
+write1 <- function(S){                           #Input is vector S
   b1 <- sample(1:length(S), 1,  prob = S/sum(S)) #randomly pick a word from b, based on the probabilities in S
-  return(b1)
+  return(b1)                                     #output is the index of words
 }
 
-write2 <- function(A1){                            #A[,w1]
+write2 <- function(A1){                            #Input is A[,w1]
   c1 <- sample(1:length(A1), 1, prob = A1/sum(A1)) ##randomly pick a word based on the probabilities in A
   return(c1)
 }
 
-write3 <- function(T12){                               #T12=T[,w1,w2]
+write3 <- function(T12){                               #Input is T12=T[,w1,w2]
   d1 <- sample(1:length(T12), 1, prob = T12/sum(T12)) ##randomly pick a word based on the probabilities in T
   return(d1)
 }
@@ -180,16 +189,16 @@ for (Aii in Ai1) {
 
 #--------------------------------------------------------------
 #8.(continue)
-for (i in 1:50){   #补注释
+for (i in 1:50){  #Loop for 50 words
   
-  if(i>2 & sum(T[,w[i-1],w[i-2]]) > 0){
-    w[i] <- write3(T[,w[i-1],w[i-2]])
-  }else if(i>1 & sum(A[,w[i-1]]) > 0){
-    w[i] <- write2(A[,w[i-1]])
-  }else{
-    w[i] <- write1(S)
+  if(i>2 & sum(T[,w[i-1],w[i-2]]) > 0){ # whether the pair of words will followed by a word, if not take write2 function
+    w[i] <- write3(T[,w[i-1],w[i-2]])   # pick the third word if an appropriate phrase exists
+  }else if(i>1 & sum(A[,w[i-1]]) > 0){  # whether a single word followed by another word, if not take write1 function
+    w[i] <- write2(A[,w[i-1]])          # pick the second word
+  }else{                               
+    w[i] <- write1(S)                   # pick the first word
   }
-  te[i] <- com[w[i]]
+  te[i] <- com[w[i]]                    # find the words in common vector from the index above
 }
 
 #a function for capitalization 
